966a2f2754150a1ddcb78491a3a6e763469f5165
Ethan McKanna <ethan.mckanna@gmail.com>
2024-11-17 00:36:08 -0600

    remove comments for now

--- functions/polyfills.js
+++
@@ -1,9 +0,0 @@
-import { Buffer } from 'buffer';
-import { Readable, Writable, Transform } from 'stream-browserify';
-import { inherits } from 'util';
-
-export { Buffer, Readable, Writable, Transform, inherits };
-
-export const process = {
-  env: { NODE_ENV: 'production' },
-};
