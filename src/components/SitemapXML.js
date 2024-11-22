abe5dbfcdd09441b8730d95336d8af9090e93259
Ethan McKanna <ethan.mckanna@gmail.com>
2024-11-22 10:23:52 -0600

    sitemap fixes

--- src/components/SitemapXML.js
+++ src/components/SitemapXML.js
@@ -1,5 +1,4 @@
-
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { db } from '../firebase';
 import { collection, query, orderBy, getDocs } from 'firebase/firestore';
 
