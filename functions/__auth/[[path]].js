ec8e508c90d51ebd719326ea68203f5f628b9112
Ethan McKanna <ethan.mckanna@gmail.com>
2024-12-02 19:21:06 -0600

    enhance request proxying with query string handling and improved logging

--- functions/__auth/[[path]].js
+++ functions/__auth/[[path]].js
@@ -1,10 +1,12 @@
 export async function onRequest({ params, request }) {
     const path = params.path ? params.path.join('/') : '';
-    const url = `https://briefsnap-76b64.firebaseapp.com/__/auth/${path}`;
+    const url = `https://briefsnap-76b64.firebaseapp.com/__/auth/${path}${request.url.includes('?') ? request.url.substring(request.url.indexOf('?')) : ''}`;
+    
+    console.log('Proxying request to:', url);
     
     return fetch(url, {
       method: request.method,
       headers: request.headers,
-      body: request.body
+      body: request.method !== 'GET' ? request.body : undefined
     });
   }
