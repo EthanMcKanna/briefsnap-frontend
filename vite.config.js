fec497fbe287d3155321ed9a27bafc66c01fb2b4
Ethan McKanna <ethan.mckanna@gmail.com>
2024-12-02 19:10:43 -0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/auth': {
-        target: 'https://briefsnap-76b64.firebaseapp.com',
-        changeOrigin: true,
-        secure: true
-      }
-    }
+    port: 3000
   },
   esbuild: {
     loader: 'jsx',
